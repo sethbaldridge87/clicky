53701a1d3907f834e7a94481455aa31b38fa688b
Seth Baldridge <sethbaldridge87@gmail.com>
2019-03-29 15:46:16 -0600

    State change added

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import "./CharacterCard.css";
 
-function CharacterCard(props) {
+class CharacterCard extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicked: "false"
+        };
+    }
+    handleClick = () => {
+        this.setState({clicked: "true"});
+    };
+    render(){
     return <div class="col l3">
-    <div className="character card" data-id={props.id}>
-            <div className="card-image">
-                <img alt={props.name} src={"images/" + props.image}/>
+            <div className="character card" data-id={this.props.id} onClick={this.handleClick}>
+                <div className="card-image">
+                    <img alt={this.props.name} src={"images/" + this.props.image}/>
+                </div>
+                <h5>{this.state.clicked}</h5>
             </div>
-    </div>
-</div>
+        </div>
+    }
 }
-
 export default CharacterCard;

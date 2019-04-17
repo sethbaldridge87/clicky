6b9610e4bd7450b298438372ddd73366b43cef7a
sethbaldridge87 <sethbaldridge87@gmail.com>
2019-04-16 21:24:36 -0600

    Updates

---
+++ precache-manifest.eed00d7a4acaa2b919b4bd2cc292869f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b83946404a49a80cabb",
+    "url": "/react_click_game/static/js/runtime~main.902fbdad.js"
+  },
+  {
+    "revision": "cd73fae3c2736031e011",
+    "url": "/react_click_game/static/js/main.935ab827.chunk.js"
+  },
+  {
+    "revision": "929afb477b46d9a816dc",
+    "url": "/react_click_game/static/js/2.8f64a495.chunk.js"
+  },
+  {
+    "revision": "cd73fae3c2736031e011",
+    "url": "/react_click_game/static/css/main.3b4e81d4.chunk.css"
+  },
+  {
+    "revision": "0c76fd83eccbfa9ebf35616e6b9a8bfe",
+    "url": "/react_click_game/index.html"
+  }
+];
